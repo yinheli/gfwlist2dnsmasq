6d4e1f66e01267905ce75b001df1116a864265b8
yinheli <me@yinheli.com>
2016-04-29 18:49:01 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# convert gfwlist to dnsmasq
+
+## install
+
+### Download binnary
+
+Download binnary from [release](/TODO)
+
+## usage
+
+```bash
+
+```
---
+++ parse_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestParse(t *testing.T) {
+	parse()
+	//	domainList := parse()
+	//	for _, domain := range domainList {
+	//		fmt.Println(domain)
+	//	}
+	//	fmt.Println(len(domainList))
+}
